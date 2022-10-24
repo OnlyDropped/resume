aa18b4a5e653767e3abcb755c1aea6b62e10f673
OnlyDropped <asheburenkov@yandex.ru>
2022-10-24 19:40:47 +0300

    added new changes in media.scss

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@ AOS.init({
 
 
 const btn = document.querySelector('.header-nav__menu');
+const nav = document.querySelector('.header-nav__info');
 btn.addEventListener('click', function() {
   btn.classList.toggle('header-nav__menu--show');
+})
+
+btn.addEventListener('click', function() {
+  nav.classList.toggle('header-nav__info--active');
 })
